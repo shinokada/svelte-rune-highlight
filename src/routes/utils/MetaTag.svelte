7a70bdbd9d4dc26dbd42442d6ed42269d494e006
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-02-23 15:48:35 +0100

    docs: meta update

--- src/routes/utils/MetaTag.svelte
+++ src/routes/utils/MetaTag.svelte
@@ -1,11 +1,11 @@
 <svelte:head>
-	<meta name="description" content="Syntax highlighting for Svelte 5 Runes" />
+	<meta name="description" content="Syntax highlighting for Svelte 5 with  Runes" />
 	<meta name="keywords" content="Svelte 5, Syntax-hightlighting, runes" />
 	<meta name="robots" content="index,follow" />
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:creator" content="@shinokada" />
 	<meta name="twitter:title" content="Svelte Rune Highlight" />
-	<meta name="twitter:description" content="Syntax highlighting for Svelte 5 Runes" />
+	<meta name="twitter:description" content="Syntax highlighting for Svelte 5 with Runes" />
 	<meta
 		name="twitter:image"
 		content="https://open-graph-vercel.vercel.app/api/svelte-rune-highlight"
@@ -14,7 +14,7 @@
 	<meta property="og:url" content="https://svelte-5-ui-lib.vercel.app/" />
 	<meta property="og:type" content="website" />
 	<meta property="og:title" content="Svelte Rune Highlight" />
-	<meta property="og:description" content="Syntax highlighting for Svelte 5 Runes" />
+	<meta property="og:description" content="Syntax highlighting for Svelte 5 with Runes" />
 	<meta
 		property="og:image"
 		content="https://open-graph-vercel.vercel.app/api/svelte-rune-highlight"
