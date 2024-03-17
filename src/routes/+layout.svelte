4c1a704f3be919f2775020a1e3a80edc14dc7dc3
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-03-17 07:33:27 +0100

    feat: add @vercel/analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,9 @@
   import MetaTag from './utils/MetaTag.svelte';
   import Nav from './utils/Nav.svelte';
   import Footer from './utils/Footer.svelte';
+  import { inject } from '@vercel/analytics';
+
+  inject();
   let { children } = $props();
 </script>
 
