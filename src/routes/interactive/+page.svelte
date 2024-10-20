571264fb472aff26233fdde4c4e71f73e63b1fa1
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-10-20 15:51:35 +0200

    chore: dependencies update

--- src/routes/interactive/+page.svelte
+++ src/routes/interactive/+page.svelte
@@ -1,14 +1,8 @@
 <script lang="ts">
-  import { Highlight, HighlightSvelte, HighlightAuto, LangTag } from '$lib';
-  import { Label, Radio, Spinner } from 'svelte-5-ui-lib';
+  import { Highlight, HighlightSvelte, LangTag } from '$lib';
+  import { Label, Radio, Spinner, type SpinnerProps } from 'svelte-5-ui-lib';
   import Codewrapper from './Codewrapper.svelte';
   import typescript from '$lib/languages/typescript';
-  import HighlightCompo from '../utils/HighlightCompo.svelte';
-  const modules = import.meta.glob('./md/*.{md,json}', {
-    query: '?raw',
-    import: 'default',
-    eager: true
-  });
   import '$lib/styles/github-dark.css';
   const sizes = ['4', '5', '6', '8', '10', '12', '16'];
   let spinnerSize = $state('8');
@@ -25,7 +19,7 @@
 <h2>HighlightSvelte</h2>
 <HighlightSvelte code={`let banana = 🍌`} />
 
-<Spinner size={spinnerSize} />
+<Spinner size={spinnerSize as SpinnerProps['size']} />
 <div class="mb-4 flex flex-wrap space-x-4">
   <Label class="mb-4 w-full font-bold">Size:</Label>
   {#each sizes as size}
